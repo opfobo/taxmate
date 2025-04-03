e64322d439f3ee4f42827f575e7a80c5f88712d0
opfobo <67360710+opfobo@users.noreply.github.com>
2025-04-04 01:04:10 +0200

    Create parsedAddress.ts

---
+++ src/lib/types/parsedAddress.ts
@@ -0,0 +1,13 @@
+export interface ParsedAddress {
+  postalCode?: string;
+  region?: string;
+  city?: string;
+  street?: string;
+  house?: string;
+  building?: string;
+  apartment?: string;
+  fullName?: string;
+  phone?: string;
+  birthDate?: string;
+  email?: string;
+}
