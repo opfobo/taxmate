5681982cec7d732e0c6e79548e82a83f9604af8b
opfobo <67360710+opfobo@users.noreply.github.com>
2025-04-07 16:11:28 +0200

    Update parseAnyAddress.ts

--- src/lib/parser/address/parseAnyAddress.ts
+++ src/lib/parser/address/parseAnyAddress.ts
@@ -1,8 +1,10 @@
-import { ParsedAddress } from "./types";
+import type { ParsedAddress } from "./types";
 import { parseCyrillicAddress } from "./parseCyrillicAddress";
 import { parseLatinAddress } from "./parseLatinAddress";
 
-export function parseAnyAddress(input: string): ParsedAddress {
+type ParsedAnyAddress = ParsedAddress | ReturnType<typeof parseCyrillicAddress>;
+
+export function parseAnyAddress(input: string): ParsedAnyAddress {
   const isCyrillic = /[А-Яа-яЁё]/.test(input);
   return isCyrillic ? parseCyrillicAddress(input) : parseLatinAddress(input);
 }
