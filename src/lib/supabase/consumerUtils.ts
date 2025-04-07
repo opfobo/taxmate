99ab97635dab134c5c007d5f4ff53f10d5c113b4
opfobo <67360710+opfobo@users.noreply.github.com>
2025-04-07 13:34:35 +0200

    Update consumerUtils.ts
    import { supabase } from "@/integrations/supabase/client";
    bereinigter import

--- src/lib/supabase/consumerUtils.ts
+++ src/lib/supabase/consumerUtils.ts
@@ -1,4 +1,4 @@
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/integrations/supabase/client";
 import { toast } from "@/components/ui/use-toast";
 
 interface ConsumerField {
