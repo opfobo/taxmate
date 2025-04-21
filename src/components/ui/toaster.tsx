44eac82b9a861d4309dee3a9559ebd6c9ad7a2ec
opfobo <67360710+opfobo@users.noreply.github.com>
2025-04-21 11:26:28 +0200

    Update toaster.tsx

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -7,20 +7,28 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
+import { CheckCircle } from "lucide-react"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider>
+    <ToastProvider duration={3000}>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
+          <Toast
+            key={id}
+            {...props}
+            className="bg-teal-50 border-l-4 border-teal-400 text-teal-900 shadow-md"
+          >
+            <div className="flex items-start gap-2">
+              <CheckCircle className="mt-0.5 w-4 h-4 text-teal-500" />
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
             </div>
             {action}
             <ToastClose />
